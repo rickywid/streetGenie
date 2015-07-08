4cc01ed630f512c4c554a8f8d37c6e7ae3923c19
Ricky JW <rickywid@hotmail.com>
2015-07-08 02:03:09 -0400

    first commit

--- styles.js
+++ styles.js
@@ -18,19 +18,19 @@ $(document).ready(function(){
 			}else if(x > 0.1 && x < 0.2){
 				$(this).before("<h1 class='answer'>I DON'T ANSWER STUPID QUESTIONS</h1>");		
 			}else if(x > 0.2 && x < 0.3){
-				$(this).before("<h1 class='answer'>I THINK YOU ALREADY KNOW THE ANSWER</h1>");		
+				$(this).before("<h1 class='answer'>ANYTHING IS POSSIBLE MY FRIEND</h1>");		
 			}else if(x > 0.3 && x < 0.4){
 				$(this).before("<h1 class='answer'>I'M DRUNK RIGHT NOW. ASK AGAIN TOMORROW</h1>");		
 			}else if(x > 0.4 && x < 0.5){
-				$(this).before('<h1 class="answer">NO WAY JOSE</h1>');		
+				$(this).before("<h1 class='answer'>I HAVE A HANGOVER. ASK AGAIN LATER ON</h1>");		
 			}else if(x > 0.5 && x < 0.6){
-				$(this).before('<h1 class="answer">YES, MOST DEFINITELY</h1>');		
+				$(this).before('<h1 class="answer">IT SURE SEEMS LIKE, DONT YOU THINK?</h1>');		
 			}else if(x > 0.6 && x < 0.7){
 				$(this).before('<h1 class="answer">DO I REALLY NEED TO ANSWER THAT?</h1>');		
 			}else if(x > 0.7 && x < 0.8){
 				$(this).before('<h1 class="answer">NEGATIVE</h1>');		
 			}else if(x > 0.9 && x < 1){
-				$(this).before('<h1 class="answer">OF COURSE, IM 100% CONFIDENT</h1>');		
+				$(this).before('<h1 class="answer">YES. I AM 100% SURE</h1>');		
 			}
 
 			$('.ask-again').delay(2000).fadeIn();
